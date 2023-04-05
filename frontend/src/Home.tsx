c8c3e9b7fae59bd3357197b8fa9c6638383868af
Chad Yorgason <chad.yorgason@gmail.com>
2023-04-05 10:58:48 -0600

    upload m13

---
+++ frontend/src/Home.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./main/App.css";
+import TopBanner from "./Blah";
+import MovieList from "./Movies";
+import { Link } from "react-router-dom";
+
+function Home(props: any) {
+  return (
+    <>
+      <div>
+        <ul className="list-group list-group-flush">
+          <>
+            <Link to="/" className="list-group-item">
+              Home
+            </Link>
+          </>
+          <>
+            <Link to="/podcast" className="list-group-item">
+              Podcast
+            </Link>
+          </>
+          <>
+            <Link to="/movies" className="list-group-item">
+              Movies List
+            </Link>
+          </>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-2">
+          <img src="./JoelHiltonHeadshot (1).jpg" alt="companyLogo" />
+        </div>
+        <div className="col-3">
+        </div>
+        <div className="col-3">
+        </div>
+        <div className="col-2">
+          <p>{props.title}</p>
+        </div>
+      </div>
+      <div className="col-4">
+        
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ frontend/src/main/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TopBanner from "../Blah";
+import MovieList from "../Movies";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Podcast from "../Podcast";
+import Home from "../Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home title="This is Joel's Movie Collection. Browse using the links" />
+            }
+          />
+          <Route path="/podcast" element={<Podcast />} />
+          <Route path="/movies" element={<MovieList />} />
+        </Routes>
+      </Router>
+      {/*<TopBanner saying="Home" />
+      <MovieList />*/}
+    </div>
+  );
+}
+
+export default App;
